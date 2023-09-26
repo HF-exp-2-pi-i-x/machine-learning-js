336a827c62d1653d4e29c5891d3a65c0bf221a44
HF-exp-2-pi-i-x <hanlolccc@gmail.com>
2023-09-26 11:27:02 +0800

    feature extract & visualization

--- common/utils.js
+++ common/utils.js
@@ -2,6 +2,17 @@ const utils = {};
 
 utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
 
+utils.styles = {
+  car: { color: "gray", text: "🚗" },
+  fish: { color: "red", text: "🐠" },
+  house: { color: "yellow", text: "🏠" },
+  tree: { color: "green", text: "🌳" },
+  bicycle: { color: "cyan", text: "🚲" },
+  guitar: { color: "blue", text: "🎸" },
+  pencil: { color: "magenta", text: "✏️" },
+  clock: { color: "lightgray", text: "🕒" },
+};
+
 utils.formatPercent = (n) => {
   return (n * 100).toFixed(2) + "%";
 };
--- web/js/display.js
+++ web/js/display.js
@@ -13,6 +13,7 @@ function createRow(container, studentName, samples) {
 
     const sampleContainer = document.createElement("div");
     sampleContainer.id = "sample_" + id;
+    sampleContainer.onclick = () => handleClick(sample, false);
     sampleContainer.classList.add("sampleContainer");
 
     const sampleLabel = document.createElement("div");
@@ -28,4 +29,30 @@ function createRow(container, studentName, samples) {
     sampleContainer.appendChild(img);
     row.appendChild(sampleContainer);
   }
+}
+
+function handleClick(sample, doScroll = true) {
+  if (sample == null) {
+    [...document.querySelectorAll(".emphasize")].forEach((e) =>
+      e.classList.remove("emphasize")
+    );
+    return;
+  }
+  const el = document.getElementById("sample_" + sample.id);
+  if (el.classList.contains("emphasize")) {
+    el.classList.remove("emphasize");
+    chart.selectSample(null);
+    return;
+  }
+  [...document.querySelectorAll(".emphasize")].forEach((e) =>
+    e.classList.remove("emphasize")
+  );
+  el.classList.add("emphasize");
+  if (doScroll) {
+    el.scrollIntoView({
+      behavior: "auto",
+      block: "center",
+    });
+  }
+  chart.selectSample(sample);
 }
